848bd7fa870a5f257b0a1775e07eac9b2b2fb91f
AkProgrammiz <akshatprasad2020@example.com>
2024-12-19 21:04:38 +0530

    Tutorial #7 till 6:05

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as sr
+import eel
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -9,4 +11,25 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("India is my country, all Indians are my brothers and sisters")
+@eel.expose
+def takecommand():
+    r=sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening...')
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+
+        audio=r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query=r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+
